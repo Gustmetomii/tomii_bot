f032bf24f9c36fa898620aac5fbc53bb902517bf
Gust me tomii <tomiielsakran1@gmail.com>
2026-05-11 17:03:06 +0300

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.environ.get('TOKEN')
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('البوت شغال يا تومي 🍓')
+async def start(update, context):
+    await update.message.reply_text('مرحباً! أنا تومي بوت جاهز للعمل!')
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
